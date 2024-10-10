07c60e9d769fedb67a13c0caca051a1e481c4445
M Makaula <221109678@tut4life.ac.za>
2024-10-10 09:27:12 +0200

    Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,3 @@
-// src/components/Footer.js
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './footer.css'; // Make sure to create or update this CSS file
@@ -7,55 +5,34 @@ import './footer.css'; // Make sure to create or update this CSS file
 const Footer = () => {
     return (
         <footer className="site-footer section-padding">
-            <div className="container">
+            <div className="container2">
                 <div className="row">
                     <div className="col-lg-3 col-12 mb-4 pb-2">
                         <Link className="navbar-brand mb-2" to="/">
-                        <i className="bi bi-mortarboard-fill graduation-icon"></i> {/* Add a custom class */}
-                           
+                            <i className="bi bi-mortarboard-fill graduation-icon"></i>
                         </Link>
                     </div>
-
                     <div className="col-lg-3 col-md-4 col-6">
-                        <h6 className="site-footer-title mb-3">Resources</h6>
+                        <h6 className="site-footer-title mb-3">Links</h6>
                         <ul className="site-footer-links">
                             <li className="site-footer-link-item">
-                                <Link to="/" className="site-footer-link">Home</Link>
+                                <Link to="#section_1" className="site-footer-link">Home</Link>
                             </li>
                             <li className="site-footer-link-item">
-                                <Link to="/how-it-works" className="site-footer-link">How it works</Link>
+                                <Link to="#section_4" className="site-footer-link">Contact Us</Link>
                             </li>
                             <li className="site-footer-link-item">
-                                <Link to="/faqs" className="site-footer-link">FAQs</Link>
-                            </li>
-                            <li className="site-footer-link-item">
-                                <Link to="/contact" className="site-footer-link">Contact</Link>
+                                <Link to="/Donate" className="site-footer-link">Donate</Link>
                             </li>
                         </ul>
                     </div>
-
                     <div className="col-lg-3 col-md-4 col-6 mb-4 mb-lg-0">
-                        <h6 className="site-footer-title mb-3">Information</h6>
-                        <p className="text-white d-flex mb-1">
-                            <a href="tel:305-240-9671" className="site-footer-link">
-                                305-240-9671
-                            </a>
+                        <h6 className="site-footer-title mb-3">Where To Find Us</h6>
+                        <p className="text-white d-flex mb-4">
+                            <a href="tel:081-355-6089" className="site-footer-link">081-355-6089</a>
                         </p>
                         <p className="text-white d-flex">
-                            <a href="mailto:info@company.com" className="site-footer-link">
-                                info@company.com
-                            </a>
-                        </p>
-                    </div>
-
-                    <div className="col-lg-3 col-md-4 col-12 mt-4 mt-lg-0 ms-auto">
-                        <p className="copyright-text mt-lg-5 mt-4">
-                            Copyright © 2024 Project X. All rights reserved.
-                            <br /><br />
-                            Design: <a rel="noopener noreferrer" href="https://templatemo.com" target="_blank">TemplateMo</a>
-                        </p>
-                        <p className="copyright-text mt-lg-5 mt-4">
-                            Editor: <a rel="noopener noreferrer" href="https://github.com/KISMETtakk" target="_blank">Project X (ICEP)</a>
+                            <a href="mailto:info@company.com" className="site-footer-link">alumnispace@tut.ac.za</a>
                         </p>
                     </div>
                 </div>
