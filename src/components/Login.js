205f0b2de752b456bc649b5d375b1a83db8a9bd5
Z Msibi <220247422@tut4life.ac.za>
2024-10-28 15:38:49 +0200

    changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -71,7 +71,7 @@ const Login = () => {
               </div>
             )}
 
-            <Link to="/forgot-password" className="forgot-password-link">
+            <Link to="/resetpassword" className="forgot-password-link">
               Forgot Password?
             </Link>
 
