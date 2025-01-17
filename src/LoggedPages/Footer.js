4b22f6198e88dc827352a7512f4618bd650e0ca5
M Makaula <221109678@tut4life.ac.za>
2025-01-17 10:05:22 +0200

    added new footer

--- src/LoggedPages/Footer.js
+++ src/LoggedPages/Footer.js
@@ -1,79 +1,118 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+ import './footer.css'; // Import your custom CSS for styling
 import { Link } from 'react-router-dom';
-import './footer.css';
-import logo from '../images/aslogo.png';
+import ALogo from '../images/aslogo.png';
 
 const Footer = () => {
+    
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+      const handleMouseMove = (e) => {
+        setCursorPosition({ x: e.clientX, y: e.clientY });
+      };
+  
+      window.addEventListener('mousemove', handleMouseMove);
+      return () => window.removeEventListener('mousemove', handleMouseMove);
+    }, []);
+
+    const [tilt, setTilt] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (e) => {
+      const logo = e.target.closest('.logo-container'); // Get the logo container
+      const { left, top, width, height } = logo.getBoundingClientRect();
+  
+      // Get mouse position relative to the logo container
+      const offsetX = e.clientX - left;
+      const offsetY = e.clientY - top;
+  
+      // Calculate tilt values based on mouse position
+      const tiltX = ((offsetY / height) - 0.5) * 30; // tilt range of -15deg to 15deg
+      const tiltY = ((offsetX / width) - 0.5) * -40; // tilt range of -15deg to 15deg
+  
+      setTilt({ x: tiltX, y: tiltY });
+    };
+    
     return (
-        <footer className="site-footer ss">
-            <div className="container22">
-                <div className="row footer-row">
-                    <div className="col-lg-3 col-12 mb-4 pb-2">
-                        <Link className="navbar-brand mb-2" to="/">
-                            <img 
-                                src={logo} 
-                                alt="Logo"
-                                style={{ width: '105px', height: '105px' }} 
-                                className="graduation-icon"
-                            />
-                        </Link>
-                    </div>
-
-                    {/* Quick Links Section */}
-                    <div className="col-lg-3 col-md-6 col-12 mb-4" id='links'>
-                        <h6 className="site-footer-title mb-3">Quick Links</h6>
-                        <ul className="site-footer-links">
-                            <li className="site-footer-link-item">
-                                <Link to="/logged#section_2" className="site-footer-linkf">About Us</Link>
-                            </li>
-                            <li className="site-footer-link-item">
-                                <Link to="/donate" className="site-footer-linkf">Donate</Link>
-                            </li>
-                            <li className="site-footer-link-item">
-                                <Link to="/logged#section_4" className="site-footer-linkf">FAQs</Link>
-                            </li>
-                        </ul>
-                    </div>
-
-                    {/* Social Media Section */}
-                    <div className="col-lg-3 col-md-6 col-12 mb-4" id='linksss'>
-                        <p className="site-footer-title mb-3" style={{ color: '#e6b012', fontSize: '25px' }}>Connect with us</p>
-                        <div className="social-mediass">
-                            <div className="social-iconss" onClick={() => window.open('https://www.facebook.com/TUTCommunications', '_blank')}>
-                                <i className="fab fa-facebook-square"></i>
-                            </div>
-                            <div className="social-iconss" onClick={() => window.open('https://x.com/official_tut', '_blank')}>
-                                <i className="fab fa-twitter-square"></i>
-                            </div>
-                            <div className="social-iconss" onClick={() => window.open('https://www.tut.ac.za', '_blank')}>
-                                <i className="fab fa-tiktok"></i>
-                            </div>
-                            <div className="social-iconss" onClick={() => window.open('https://www.linkedin.com/company/official-tshwane-university-of-technology/posts/?feedView=all', '_blank')}>
-                                <i className="fab fa-linkedin"></i>
-                            </div>
-                            <div className="social-iconss" onClick={() => window.open('https://www.youtube.com/channel/UCD4jxDpRYTarILQjtsEQv9Q?view_as=subscriber', '_blank')}>
-                                <i className="fab fa-youtube-square"></i>
-                            </div>
-                            <div className="social-iconss" onClick={() => window.open('https://www.instagram.com/tut_official2/#', '_blank')}>
-                                <i className="fab fa-instagram-square"></i>
-                            </div>
-                        </div>
-                    </div>
+      <footer className="fo-footer_section">
+        <div className="fo-widget_wrapper">
+          <div className="container">
+            <div className="row">
+              {/* Logo and Social Media Section */}
+              <div className="col-lg-6 col-md-6 col-12">
+                <div className="fo-widget fo-widget_about">
+                  <div className="logo-container" onMouseMove={handleMouseMove}>
+                    <img
+                      src={ALogo}
+                      className="hover-logo"
+                      alt="Animated Logo"
+                      style={{
+                        // transform: `rotateX(${tilt.x}deg) rotateY(${tilt.y}deg)`,
+                      }}
+                    />
+                  </div>
+                  <ul className="fo-social">
+                    <li><a href="https://www.facebook.com/TUTCommunications"><i className="fab fa-facebook-f"></i></a></li>
+                    <li><a href="https://x.com/official_tut"><i className="fab fa-twitter"></i></a></li>
+                    <li><a href="https://www.instagram.com/tut_official2/#"><i className="fab fa-instagram"></i></a></li>
+                    <li><Link to="/notfoundpage"><i className="fab fa-tiktok"></i></Link></li>
+                    <li><a href="https://www.linkedin.com/company/official-tshwane-university-of-technology/posts/?feedView=all"><i className="fab fa-linkedin"></i></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCD4jxDpRYTarILQjtsEQv9Q?view_as=subscriber"><i className="fab fa-youtube-square"></i></a></li>
+                  </ul>
                 </div>
+              </div>
 
-                
-                <div className="row">
-                    <div className="col-12 text-center">
-                        <p className="footer-copyright">
-                            Copyright © 2024 TUT - All Rights Reserved
-                            <span className="privacy-policy"> | Privacy Policy & POPIA</span>
-                        </p>
-                        
+              {/* Quick Links Section */}
+              <div className="col-lg-2 col-md-6 col-sm-12">
+                <div className="fo-widget fo-widget_links">
+                  <div className="fo-widget_title">
+                    <h4>Quick Links</h4>
+                  </div>
+                  <div className="fo-contact_info">
+                    <div className="fo-single_info">
+                      <div className="fo-icon foot-icon-size-up icon-animation1">
+                        <i className="fa-solid fa-circle-info "></i>
+                      </div>
+                      <div className="fo-info">
+                        <p className="site-footer-linkf"><Link to="/logged#section_2">About Us</Link></p>
+                      </div>
                     </div>
+                    <div className="fo-single_info">
+                      <div className="fo-icon foot-icon-size-up icon-animation2">
+                        <i className="fa-solid fa-hand-holding-dollar"></i>
+                      </div>
+                      <div className="fo-info">
+                        <p className="site-footer-linkf"><Link to="/donate">Donate</Link></p>
+                      </div>
+                    </div>
+                    <div className="fo-single_info">
+                      <div className="fo-icon foot-icon-size-up icon-animation3">
+                        <i className="fa-solid fa-person-circle-question"></i>
+                      </div>
+                      <div className="fo-info">
+                        <p className="site-footer-linkf"><Link to="/logged#section_4" >FAQs</Link></p>
+                      </div>
+                    </div>                    
+                  </div>
                 </div>
-                
+              </div>
             </div>
-        </footer>
+          </div>
+
+          {/* Copyright Section */}
+          <div className="fo-copyright_area">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="fo-copyright_text">
+                    <p>Copyright &copy; 2024 TUT All Rights Reserved<span className='p-footer-space'>|</span>Privacy Policy & POPIA</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     );
 };
 
